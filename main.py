5c9d64b56cbd6257691a29ad1e064ac971a0d88a
Pawel P <peempe58@gmail.com>
2021-09-23 14:46:05 +0200

    Dodano przyjecie i wypisanie danych wejsciowych

---
+++ main.py
@@ -0,0 +1,22 @@
+# Program do wyliczania pozostalej kwoty kredytu.
+# Miesieczna kapitalizacja odsetek.
+# Program powinien przyjmowac z terminala:
+#       - kwote kredytu
+#       - oprocentowanie (w procentach) powyzej inflacji
+#       - wysokosc stalej miesiecznej raty
+# Wartosci inflacji w kazdym miesiacu program powinien przyjmowac z pliku.
+# Program zwraca wartosci dla 24 kolejnych miesiecznych rat w postaci:
+#   <miesiac>: Twoja pozostala kwota kredytu: <X>. To o <Y> mniej niz
+#   w poprzednim miesiacu."
+#   -------------------------------------------------------------------------
+#
+#  przyjecie danych kredytu:
+wys_kredytu = input('Podaj wysokosc kredytu: ')
+wys_oprocentowania = input('Podaj oprocentowanie ponad inflacje: ')
+wys_stalej_raty = input('Podaj wysokosc raty: ')
+
+# wypisanie danych startowych kredytu
+print('\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+print('\nINFO:\n  Twoj kredyt: {} PLN\n  Oprocentowanie: {}%\n  Rata: {} PLN'
+      .format(wys_kredytu, wys_oprocentowania, wys_stalej_raty))
+print('\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
